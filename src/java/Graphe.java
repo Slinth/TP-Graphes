df22baa918ecd4f963bf752f59e2e68a974ab68f
AntoineBdns <antoine.boudens@gmail.com>
2018-12-12 14:48:25 +0100

    Création de la fonction d'init pour le graphe

--- src/java/Graphe.java
+++ src/java/Graphe.java
@@ -2,13 +2,38 @@ import java.util.ArrayList;
 
 
 class Graphe{
-	public ArrayList<Sommets> data;
+	public ArrayList<Sommet> data = new ArrayList();
 	public int nbSommets;
 	public int nbArcs;
+	public static int poidsMax = 100;
+	public static int poidsMin = 1;
 
-	public Graphe(int _nbSommets,int _nbArcs){
-		this.nbSommets = nbSommets;
-		this.nbArcs = nbArcs;
-		this.construire();
+	public Graphe(int _nbSommets, int _nbArcs){
+		this.nbSommets = _nbSommets;
+		this.nbArcs = _nbArcs;
+	}
+
+	public void init(){
+		for(int i = 0 ; i < nbSommets ; i++){
+			data.add(new Sommet(i));
+		}
+
+		int cptArcs = 0;
+
+		while(cptArcs < this.nbArcs){
+			int indSommetSource = (int)(Math.random() * this.nbSommets );
+			int indSommetDestination = (int)(Math.random() * this.nbSommets );
+			int poids = poidsMin + (int)(Math.random() * ((poidsMax - poidsMin) + 1));;
+			System.out.println("Arc entre " + indSommetSource + " et " + indSommetDestination + " de poids " + poids );
+			if(indSommetDestination != indSommetSource){
+				
+				Sommet sSource = data.get(indSommetSource);
+				Sommet sDesti = data.get(indSommetDestination);
+				Arc a = new Arc (sSource,sDesti,poids);
+				sSource.addVoisin(sDesti,poids);
+				System.out.println("Arc entre " + indSommetSource + " et " + indSommetDestination + " de poids " + poids );
+				cptArcs ++ ;
+			}
+		}
 	}
 }
--- src/java/Sommet.java
+++ src/java/Sommet.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 class Sommet{
 	public int valeur;
-	public ArrayList<Arc> voisins;
+	public ArrayList<Arc> voisins = new ArrayList();
 
 	public Sommet(int _valeur){
 		this.valeur = _valeur;
