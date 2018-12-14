5141a9232aa6acb4305800e33983e7e908f4894f
AntoineBdns <antoine.boudens@gmail.com>
2018-12-14 12:06:46 +0100

    [ADD] Fonction JOHNSON ( non fonctionnel due a une sortie de tableau dans la fonction Bellman )

--- src/java/Graphe.java
+++ src/java/Graphe.java
@@ -130,4 +130,87 @@ class Graphe{
 
 		return d;	
 	}
+
+	public int[] dijkstra(Sommet s,int[][] couts){
+		int n = this.nbSommets;
+		int d[] = new int[n];
+		ArrayList<Sommet> m = new ArrayList();
+
+		for(int i = 1 ; i < this.nbSommets ; i++) d[i] = INFINI;
+
+		d[s.valeur] = 0;
+		m.add(s);
+
+		while(m.size() != this.nbSommets){
+			//Sommet appartenant pas a m ayant d(x) minimale
+			Sommet sMin = null;
+			for (Sommet so : this.data) {
+				if(!m.contains(s)){
+					if(sMin == null)sMin = s;
+					else if(d[sMin.valeur] > d[s.valeur])sMin = s;
+				}
+			}
+
+			m.add(sMin);
+
+			for (Sommet som : this.data) {
+				for (Arc a : som.voisins) {
+					int x = a.sommetSource.valeur;
+					int y = a.sommetDestination.valeur;
+					int p = couts[x][y];
+					d[y] = Math.min(d[y], p + d[x]);
+				}		
+			}
+		}
+
+		return d;	
+	}
+
+	public int[][] getTabCouts(){
+		int[][] c = new int[this.nbSommets][this.nbSommets];
+
+		for ( Sommet s : data ) {
+			for ( Arc a : s.voisins ) {
+				c[a.sommetSource.valeur][a.sommetDestination.valeur] = a.poids;
+			}
+		}
+
+		return c;
+	}
+	/**
+	 * Algorithme de Johnson
+	 * @return Matrice ou M[i][j] =  coût du plus court chemin de i à j
+	 */
+	public int[][] johnson(){
+		int[][] c = this.getTabCouts();
+		int[][] distance = new int[this.nbSommets][this.nbSommets];
+		Sommet q = new Sommet(this.nbSommets+1);
+		for ( Sommet s :this.data ) {
+			q.addVoisin(s,0);
+		}
+
+		ArrayList<Sommet> data2 = this.data;
+		data2.add(q);
+
+		Graphe G2 = new Graphe(this.nbSommets,this.nbArcs,data2,this.poidsMin,this.poidsMax);
+
+		int[] h = G2.bellmanFord(q.valeur);
+
+		int[][] c2 = new int[this.nbSommets][this.nbSommets];
+
+		for (int i = 0 ; i < this.nbSommets ; i++ ) {
+			for(int j = 0 ; j <this.nbSommets ; j++ ){
+				c2[i][j] = c[i][j] - h[i] + h[j];
+			}
+		}
+
+		for (Sommet som : this.data ) {
+			int[] d = dijkstra(som,c2);
+			for ( Sommet sommet : this.data) {
+				distance[som.valeur][sommet.valeur] = d[sommet.valeur] + h[sommet.valeur] - h[som.valeur];
+			}
+		}
+
+		return distance;
+	}
 }
